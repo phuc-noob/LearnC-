5c51859a95a2c9341d1c692a435a1a5ff3ba3939
phuc-noob <phucnooob@gmail.com>
2020-04-03 22:09:51 +0700

    C#

---
+++ FuncCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FuncCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            Console.WriteLine("Hello World!");
+            Log();
+
+            //ref will take address memory of value used to don't need copy 
+            Console.WriteLine(value);
+            IncreaseValue(ref value);
+            Console.WriteLine(value);
+
+            Console.ReadKey();
+
+        }
+
+        static void Log()
+        {
+            Console.WriteLine("function static in C# .");
+        }
+
+        // ref and out in C#
+        static void IncreaseValue(ref int value)
+        {
+            ++value;
+        }
+    }
+}
